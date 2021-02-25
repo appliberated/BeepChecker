4e6d1d577f091e6c31a1b464e747821dc48dc57c
devappliberated <dev@appliberated.com>
2021-02-25 17:32:41 +0200

    Initial commit of existing source code

---
+++ Source/BeepChecker/Program.cs
@@ -0,0 +1,32 @@
+﻿//----------------------------------------------------------------------------
+//
+// <copyright file="Program.cs" company="Appliberated">
+// Copyright (c) 2013-2021 Appliberated
+// https://www.appliberated.com
+// Licensed under the MIT. See LICENSE file in the project root for full license information.
+// </copyright>
+//
+//---------------------------------------------------------------------------
+
+namespace BeepChecker
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main program class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
